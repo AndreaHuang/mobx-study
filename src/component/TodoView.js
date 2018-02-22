42b4da50a3cae57b01ef517d613a4e88cf8dc72b
Andrea Huang <hyfandrea@gmail.com>
2018-02-23 01:40:11 +0800

    TodoView not refreshed

--- src/component/TodoView.js
+++ src/component/TodoView.js
@@ -1,29 +1,41 @@
-import React,{Component} from "react"
+import React,{Component} from "react";
 
+import {extendObservable,observable} from "mobx";
 class TodoView extends Component {
+  constructor(props){
+    super(props);
+    extendObservable(this,{
+    todo: this.props.todo,
+    count:0
+   })
+  }
   render() {
-    const todo = this.props.todo;
+    
     return (
       <li onDoubleClick={ this.onRename }>
         <input
           type='checkbox'
-          checked={ todo.completed }
+          checked={ this.todo.completed }
           onChange={ this.onToggleCompleted } />
   		   
-          { todo.task }
-          { todo.assignee
-          ? <small>{ todo.assignee.name }</small>
+          { this.todo.task }
+          { this.todo.assignee
+          ? <small>{ this.todo.assignee.name }</small>
           : null
         }
+        {this.count}
 
       </li>
     );
   }
 
   onToggleCompleted = () => {
-    console.log("onToggleCompleted",this.props.todo.completed );
-    const todo = this.props.todo;
+    console.log("onToggleCompleted",this.todo.completed );
+    const todo = this.todo;
     todo.completed = !todo.completed;
+    this.todo=todo;
+    this.count++;
+    console.log(this.count);
   }
 
   onRename = () => {
